633d9ff51e6d22f513fdc8cc7b73dbcac126a719
Matthew Olson <Olsonm811@gmail.com>
2024-10-13 21:19:02 -0400

    testing script.js

--- projects/part6/script.js
+++ projects/part6/script.js
@@ -1,2 +1,16 @@
 /*Toggle the navagation on smaller screens*/
 /*Was not able to complete it this week, but will have it next week*/
+const getLocations = async() => {
+    const url = "./locations.json";
+
+    try {
+        const response = await fetch(url);
+        console.log(response);
+        const data = await response.json();
+        console.log(data);
+        return data;
+    } catch(error){
+        console.log(error);
+    }
+};
+getLocations();
